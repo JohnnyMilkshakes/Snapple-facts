2c1f74de335b3f1f5de65cda25e1f04807055a87
Jonathan C <johnnymilkshakes@icloud.com>
2024-08-04 14:42:17 -0400

    update models to match ERD

--- models/snapple-fact.js
+++ models/snapple-fact.js
@@ -1,14 +1,22 @@
 import mongoose from "mongoose"
+const Schema = mongoose.Schema
 
-const snappleFactSchema = new mongoose.Schema({
-    fact: String,
-    number: Number,
-    isRetired: Boolean,
-    // comments: [commentSchema],
-    // stars: [ObjectID],
-    source: Array
-})
-
+const CommentSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    comment: { type: String, required: true },
+    Source: [{ type: String, required: true }],
+    Date: { type: Date, required: true }
+  });
+  
+  const snappleFactSchema = new mongoose.Schema({
+    fact: { type: String, required: true },
+    number: { type: Number, required: true },
+    isRetired: { type: Boolean, required: true },
+    Source: [{ type: String, required: true }],
+    Comments: [CommentSchema], // Embedding comments
+    Stars: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Referencing users in a list to track stars
+    DiscoveredBy: {type: Schema.Types.ObjectId, ref: 'User'}, // Referencing a possible user who discovered the fact
+  });
 const SnappleFact = mongoose.model('Snapplefact', snappleFactSchema)
 
 export default SnappleFact
--- models/user.js
+++ models/user.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose"
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    Comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    Stars: [{ type: Schema.Types.ObjectId, ref: 'SnappleFact' }],
+    DiscoveredFacts: [{ type: Schema.Types.ObjectId, ref: 'SnappleFact' }] // Referencing a potential list of discovered facts
+  });
 
 const User = mongoose.model("User", userSchema);
 
